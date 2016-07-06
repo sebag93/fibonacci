59c31ac06464991bd75967605727f735d7284cf6
sebag93 <sebag93@gmail.com>
2016-07-06 14:22:29 +0200

    TEST #3

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -68,8 +68,8 @@ namespace fibonacci
             Console.WriteLine("1 - ciag fibonacciego");
             Console.WriteLine("2 - silnia");
             Console.WriteLine("3 - logowanie do bazy");
-            Console.WriteLine("");
-            Console.WriteLine("");
+            Console.WriteLine("@@@@@@@@@@@@@@@@");
+            Console.WriteLine("##############");
             int znak;
             znak = int.Parse(Console.ReadLine());
             switch (znak)
