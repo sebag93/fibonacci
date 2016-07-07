c53ea0e47ce0b7b8578df4e618e9ac5affa40896
sebag93 <sebag93@gmail.com>
2016-07-07 10:12:33 +0200

    ASDF22222

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -68,7 +68,7 @@ namespace fibonacci
             Console.WriteLine("1 - ciag fibonacciego");
             Console.WriteLine("2 - silnia");
             Console.WriteLine("3 - logowanie do bazy");
-            Console.WriteLine("@@@@@@@@@@@@@@@@");
+            Console.WriteLine("@@asdasdad@");
             Console.WriteLine("##############");
             int znak;
             znak = int.Parse(Console.ReadLine());
