916a8553427cf8959eb72a357dafb18ef231ca64
sebag93 <sebag93@gmail.com>
2016-07-18 09:01:32 +0200

    pipelinetest #2

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -125,6 +125,8 @@ namespace fibonacci
                     }
                 default:
                     Console.WriteLine("###123");
+                    Console.WriteLine("###123");
+                    Console.WriteLine("###123");
                     break;
             }
 
