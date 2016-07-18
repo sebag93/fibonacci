3088b7f7345b5f00572551f0f5eb0527eb182e52
sebag93 <sebag93@gmail.com>
2016-07-18 14:50:20 +0200

    pipeline #11374

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -125,10 +125,6 @@ namespace fibonacci
                     }
                 default:
                     Console.WriteLine("###123");
-                    Console.WriteLine("###123");
-                    Console.WriteLine("###123");
-                    Console.WriteLine("###123");
-
                     break;
             }
 
