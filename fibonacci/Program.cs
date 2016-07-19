17951e3619c3ccaeb50c89021c6cab5455ab8fad
sebag93 <sebag93@gmail.com>
2016-07-19 11:58:14 +0200

    test #11407

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -124,7 +124,7 @@ namespace fibonacci
                         break;
                     }
                 default:
-                    Console.WriteLine("###aqwe#");
+                    Console.WriteLine("###aqwedead#");
                     Console.WriteLine("#123");
                     Console.WriteLine("a 12 123");
                     break;
