ec2b39737b2abc25e3e7d3c67664e083990710bb
sebag93 <sebag93@gmail.com>
2016-07-20 16:44:19 +0200

    #11407

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -125,7 +125,7 @@ namespace fibonacci
                     }
                 default:
                     Console.WriteLine("## #1123ss");
-                    Console.WriteLine("#weq2xerw2asd");
+                    Console.WriteLine("#weq2xer35w2asd");
                     Console.WriteLine("a 1s2asdfrsssrw2w2 123");
                     break;
             }
