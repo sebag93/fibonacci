6da41902c5a35b8ff1b1142971be51cfdf62f501
sebag93 <sebag93@gmail.com>
2016-07-25 15:19:44 +0200

    jira #123 track #15 REDMINE #11407 redmine #11374

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -124,7 +124,7 @@ namespace fibonacci
                         break;
                     }
                 default:
-                    Console.WriteLine("## 2221342wess1232aa a1     ");
+                    Console.WriteLine("## 2221342wess1232aasdasa a1     ");
                     Console.WriteLine("#  2s 23w eq2d");
                      
                     break;
