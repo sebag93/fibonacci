a6b3bc8a079f0f5bf9f55565ebf5c03a05c1dd64
sebag93 <sebag93@gmail.com>
2016-07-26 10:35:24 +0200

    fix #11407

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -124,7 +124,7 @@ namespace fibonacci
                         break;
                     }
                 default:
-                    Console.WriteLine("## sads11sxa a1     ");
+                    Console.WriteLine("## sads11sxsa a1     ");
                     Console.WriteLine("#  2s 23w eq2d");
                      
                     break;
