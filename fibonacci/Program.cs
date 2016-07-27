16ca49c368f0311e9506037dc6c0744b76d4360f
sebag93 <sebag93@gmail.com>
2016-07-27 12:39:12 +0200

    test regex #11407 refs #6000, #11374 te bugi powinny zostac zignorowane track:#123, jira#123 ale te juz nie #9645, #8651

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -124,7 +124,7 @@ namespace fibonacci
                         break;
                     }
                 default:
-                    Console.WriteLine("##swws");
+                    Console.WriteLine("##swwssss");
                     Console.WriteLine("#  2s 23w eq2d");
                      
                     break;
