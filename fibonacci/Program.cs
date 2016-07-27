4e64f6bf1cc905a7072ef5ff558fa45570be1e16
sebag93 <sebag93@gmail.com>
2016-07-27 14:35:01 +0200

    #11374 #11407 #6000 #9652 #10502 #8651 #6002 #5726 #7482 #6790

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -125,7 +125,7 @@ namespace fibonacci
                     }
                 default:
                     Console.WriteLine("##swwssssssssssaa");
-                    Console.WriteLine("#  2s 23sssdw eq2d");
+                    Console.WriteLine("#  2s 23sssdssw eq2d");
                      
                     break;
             }
